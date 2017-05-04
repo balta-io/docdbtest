166a955378684a50353697dfc4d00bef412ef74b
André Baltieri <andrebaltieri@Andres-MacBook-Pro.local>
2017-05-04 17:24:05 -0300

    Implementado save no DocumentDb

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using DocumentDbTest.Domain.Entities;
 using DocumentDbTest.Domain.ValueObjects;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 
 namespace DocumentDbTest
 {
@@ -8,13 +11,21 @@ namespace DocumentDbTest
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.GetStartedDemo().Wait();
+
+            Console.ReadKey();
+        }
+
+        private async Task GetStartedDemo()
+        {
             // Gera um novo cliente
-            var customer = new Customer(new Name("André", "Baltieri"), new Document("57673386801"), new Email("hello@balta.io"));
+            var customer = new Customer(new Name("André", "Baltieri"), new Domain.ValueObjects.Document("57673386801"), new Email("hello@balta.io"));
             var mouse = new Product("Apple Magic Mouse", 599M, 2);
 
             // Gera um novo pedido
             var order = new Order();
-            order.AddItem(new OrderItem(mouse, 5));
+            order.AddItem(new OrderItem(mouse, 2));
 
             // Percorre todos os itens do pedido
             foreach (var item in order.Items)
@@ -26,9 +37,22 @@ namespace DocumentDbTest
                 }
             }
 
-            
+            await Save(order);
+        }
 
-            Console.ReadKey();
+        private async Task Save(object obj)
+        {
+            try
+            {
+                var EndpointUri = "https://balta.documents.azure.com:443/";
+                var PrimaryKey = "dTnWdd23isTXYFewhJk8j9viGASNbCzXMwYlFbtdSLfTmHYsUyhuzxQXumh2zJzLTvCZe3TapcIN9WO3aNEsWw==";
+                var client = new DocumentClient(new Uri(EndpointUri), PrimaryKey);
+                await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("balta", "orders"), obj);
+            }
+            catch (DocumentClientException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
